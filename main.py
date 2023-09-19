764f036853172d42bb3f4ebbcd12f39e3de8acbd
mabdulker <ma.abdulkerimov@gmail.com>
2023-09-19 02:20:12 +0200

    commit message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 
 # how far back you want to fill commit graph
-START = 160
+START = 200
 DAYS = 40
 # min and max amount of commits on a given day
 MIN_VOLUME = 1
