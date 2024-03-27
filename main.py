c00717392de9ed5a76f69d8c352d033612d2022e
mabdulker <ma.abdulkerimov@gmail.com>
2024-03-27 01:15:21 +0100

    commit message

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 from random import randint
 
 # how far back you want to fill commit graph
-DAYS = 300
+DAYS = 30
 # min and max amount of commits on a given day
 MIN_VOLUME = 0
 MAX_VOLUME = 20
 
 
 def greeny():
-    for i in range(1, DAYS):
+    for i in range(10, DAYS):
         for j in range(0, randint(MIN_VOLUME, MAX_VOLUME)):
             d = f"{i} days ago"
             with open("file.txt", "a") as f:
